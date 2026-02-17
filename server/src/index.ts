5e077f4f684a26e13922bd4963235b637fcaac44
E-LiyonCodeSantex <eelyondesign@gmail.com>
2026-02-17 01:36:23 +0100

    Updated server/src/index.ts: allow for deploy url from vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,7 +25,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    "http://localhost:3000",
+    "https://empire-fragrance-akwq.vercel.app/"
+  ],
   credentials: true
 }));
 app.use(express.json());
